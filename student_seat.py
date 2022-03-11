5490c07968f5dfdd8ccf623ebea3dabb78425210
“bickey1119”~ <k994958@kansai-u.ac.jp>
2022-03-11 19:46:10 +0900

    閉じるボタン削除、テキストとメッセの文字をフォントで大きくした

--- student_seat.py
+++ student_seat.py
@@ -17,23 +17,24 @@ class Application(tkinter.Frame): #tkinterのフレームを継承
     
     
     def create_widgets(self):
+        '''
         quit_btn=tkinter.Button(self)
         quit_btn['text']='閉じる'
         quit_btn['command']=self.root.destroy
         quit_btn.pack(side='bottom') #bottomで一番下に設定
+        '''
 
-        self.text_box=tkinter.Entry(self, width = 20)
-        self.text_box.place(x=10,y=10,width=200,height=300)
-        #self.text_box['width']=15
+        self.text_box=tkinter.Entry(self, justify='center', width = 10, font = b)
         self.text_box.pack()
 
-        submit_btn=tkinter.Button(self)
+        submit_btn=tkinter.Button(self, justify='center', width = 3, font = a)
         submit_btn['text']='実行'
         submit_btn['command']=self.save_data
         submit_btn.pack()
 
-        self.message=tkinter.Message(self)
+        self.message=tkinter.Message(self, aspect=500, justify='center', font=b)
         self.message.pack()
+
 
     
     def save_data(self):
@@ -49,9 +50,10 @@ class Application(tkinter.Frame): #tkinterのフレームを継承
         wb.save(file_path) #上書き保存
         student_name = ws['C'+str(retsu_number)]
         #self.message['text']=student_name.value+'さんの席番号は '+str(seat_number.value)+' です。' #生徒氏名がVLOOKUPの式になってまう…属性valueでセルの値を取得、あとメッセージ幅広げたい
-        self.message['text']='生徒番号'+text+'の席番号は '+str(seat_number.value)+' です。'
+        self.message['text']='生徒番号'+text+'の席番号は\n ' +str(seat_number.value)+'番です。'
         self.text_box.delete(0, tkinter.END) #テキストボックス内を消す
         retsu_number = retsu_number + 1 #列番号を下にずらし、入力値をずらす
+        
     
 
 
@@ -61,7 +63,7 @@ root=tkinter.Tk()  #オブジェクト作成
 root.title('座席管理表')
 root.geometry('800x400')
 retsu_number = 3
-#a = ("Arial black", 20, "bold")#最高・最低気温を表すフォント　フォント名、大きさ、太字
-#b = ("Arial black", 40, "bold")
+a = ("Arial black", 15, "bold")
+b = ("Arial black", 30, "bold")
 app=Application(root=root)
 app.mainloop()  #実行
